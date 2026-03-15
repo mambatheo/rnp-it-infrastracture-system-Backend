1878032e98ffde644176df1a49f912350353deba
mambatheo <muthegene61@gmail.com>
2026-03-15 08:44:52 +0200

    Final

--- itinfra/settings.py
+++ itinfra/settings.py
@@ -12,8 +12,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")  # FIX 1
-
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")  
 INSTALLED_APPS = [
     'admin_interface',
     'colorfield',
