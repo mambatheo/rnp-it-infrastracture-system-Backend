eb9547c4443fb173b427283d0d77d755a3c8de5e
mambatheo <muthegene61@gmail.com>
2026-03-17 15:09:38 +0200

    Settings changed

--- itinfra/settings.py
+++ itinfra/settings.py
@@ -133,7 +133,7 @@ CLOUDINARY_STORAGE = {
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
