1f375feb236fd3642b4ea2f6700b783338da7dbc
mambatheo <mutheogene61@gmail.com>
2026-03-31 16:01:16 +0200

    New Changes

--- itinfra/settings.py
+++ itinfra/settings.py
@@ -75,7 +75,7 @@ DATABASES = {
         'ENGINE':   'django.db.backends.postgresql',
         'NAME':     os.getenv("POSTGRES_DB",       'itinfra'),
         'USER':     os.getenv("POSTGRES_USER",     'postgres'),
-        'PASSWORD': os.getenv("POSTGRES_PASSWORD", ''),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD", 'Theo@1234!'),
         'HOST':     os.getenv("DB_HOST",           'db'),
         'PORT':     os.getenv("DB_PORT",           '5432'),
     }
