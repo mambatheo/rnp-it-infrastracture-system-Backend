c0e1bb80f853f30ffbf04b259f01833b6237e6fa
mambatheo <mutheogene61@gmail.com>
2026-03-31 16:23:40 +0200

    Files Removed

--- itinfra/settings.py
+++ itinfra/settings.py
@@ -72,14 +72,16 @@ WSGI_APPLICATION = 'itinfra.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE':   'django.db.backends.postgresql',
-        'NAME':     os.getenv("POSTGRES_DB",       'itinfra'),
-        'USER':     os.getenv("POSTGRES_USER",     'postgres'),
-        'PASSWORD': os.getenv("POSTGRES_PASSWORD", 'Theo@1234!'),
-        'HOST':     os.getenv("DB_HOST",           'db'),
-        'PORT':     os.getenv("DB_PORT",           '5432'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("POSTGRES_DB", "itinfra"),
+        'USER': os.getenv("POSTGRES_USER", "postgres"),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD"), 
+        'HOST': os.getenv("DB_HOST", "db"),
+        'PORT': os.getenv("DB_PORT", "5432"),
     }
 }
+
+
 
 
 
